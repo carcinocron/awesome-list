8a3a9d806eb8bd36d13034b0783a3d6d9e4ff0b9
InstanceOfMichael <InstanceOfMichael@users.noreply.github.com>
2017-03-14 10:58:50 -0500

    Create bash_template.sh

---
+++ bash_template.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [[ !$(/usr/bin/id -u) -ne 0 ]]; then
+    echo "Do Not Run This As Root"
+    exit
+fi
+# learn a new trick: https://dev.to/thiht/shell-scripts-matter
+
+set -e # halt on error
+set -x # echo cmds before executing
+set -u # fail on undefined variable
+set -o pipefail # force pipelines to fail on fail
+
